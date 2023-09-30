8c68d97b73a69e3bdfda9d94dab0f70d17a1c7c4
RayanSabbar <219110308@psu.edu.sa>
2023-09-30 21:38:51 +0300

    Update tests.py

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
-
+from .models import Account
 # Create your tests here.
+class Account(TestCase):
+    def setUp(self):
+        account = Account.objects.create_user(email='student@psu.edu.sa')
+        student = Account.objects.create_user(is_student=True)
+
+    def test_get_account(self):
+        account = Account.objects.all()[0]
+        email = Account.objects.get(email='student@psu.edu.sa')
+        response = Account.get_account(email='student@psu.edu.sa')
+        self.assertIsNotNone(response)
+
+    def test_get_student_accounts(self):
+        student = Account.objects.all()[0]
+        student = Account.object.get(is_student=True)
+        self.assertIsNotNone(response)
+
+
